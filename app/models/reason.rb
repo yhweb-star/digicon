13ebcfac878f8b5a2572c669c4ba0500b0f31d6b
yhweb-star <yh48121229@gmail.com>
2021-05-20 23:49:56 +0900

    absences/newのビュー調整

--- app/models/reason.rb
+++ app/models/reason.rb
@@ -1,8 +1,8 @@
 class Reason < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '遅刻' },
-    { id: 3, name: '欠席' },
+    { id: 2, name: '欠席' },
+    { id: 3, name: '遅刻' },
     { id: 4, name: '早退' }
   ]
 
